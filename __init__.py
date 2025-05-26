76e901e0a7b9e20b32d740d76f677ab0c3f0c6f1
labpenelitian <penelitian@usu.ac.id>
2025-05-26 11:08:51 +0700

    overriding prev commit

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import os
 import requests
 import json 
+import random
+import string 
 from flask import Flask, render_template, flash, redirect, url_for, request, jsonify
 from flaskr.util.helpers import upload_file_to_s3, get_file, show_image, show_video
 
@@ -13,6 +15,12 @@ headers = {
     "Authorization" : f"Bearer {api_key}",
     "Content-Type": "application/json"
 }
+
+def rand_string(): 
+    length = 16
+    random_string = ''. join(random.choices(string.ascii_letters + string.digits, k=length))
+
+    return random_string
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -45,89 +53,91 @@ def create_app(test_config=None):
 
     @app.route('/', methods=['GET', 'POST'])
     def hello():
-        return render_template('landing_page.html')
+        return render_template('upload.html')
         # return render_template('index.html', result = result, result_video = result_video)
 
 
     # # Define the '/upload' route inside the app context
-    # @app.route('/upload', methods=['POST'])
-    # def create():
-    #     if 'user_file' not in request.files:
-    #         flash('No user_file key in request.files')
+    @app.route('/run', methods=['GET'])
+    def run():
+      return render_template('request.html')
+        # if 'user_file' not in request.files:
+        #     flash('No user_file key in request.files')
 
-    #     file = request.files['user_file']
-    #     data = {
-    #           "input": {
-    #               "workflow": {
-    #               "10": {
-    #                   "inputs": {
-    #                     "video": f"https://runpod-hizkia-fileupload.s3.ap-southeast-1.amazonaws.com/{file.filename}",
-    #                     "force_rate": 0,
-    #                     "force_size": "Disabled",
-    #                     "custom_width": 512,
-    #                     "custom_height": 512,
-    #                     "frame_load_cap": 0,
-    #                     "skip_first_frames": 0,
-    #                     "select_every_nth": 1
-    #                   },
-    #                   "class_type": "VHS_LoadVideoPath",
-    #                   "_meta": {
-    #                     "title": "Load Video (Path) 🎥🅥🅗🅢"
-    #                   }
-    #                 },
-    #                 "12": {
-    #                   "inputs": {
-    #                     "a": 6.283185307179586,
-    #                     "bg_threshold": 0.1,
-    #                     "resolution": 512,
-    #                     "image": [
-    #                       "10",
-    #                       0
-    #                     ]
-    #                   },
-    #                   "class_type": "MiDaS-DepthMapPreprocessor",
-    #                   "_meta": {
-    #                     "title": "MiDaS Depth Map"
-    #                   }
-    #                 },
-    #                 "14": {
-    #                   "inputs": {
-    #                     "filename_prefix": "ControlNet",
-    #                     "images": [
-    #                       "12",
-    #                       0
-    #                     ]
-    #                   },
-    #                   "class_type": "SaveImageS3",
-    #                   "_meta": {
-    #                     "title": "Save Image to S3"
-    #                   }
-    #                 }
-    #               }
-    #           }
-    #       }
+        # file = request.files['user_file']
+        # prefix = rand_string()
+        # data = {
+        #       "input": {
+        #           "workflow": {
+        #           "10": {
+        #               "inputs": {
+        #                 "video": f"https://runpod-hizkia-fileupload.s3.ap-southeast-1.amazonaws.com/{file.filename}",
+        #                 "force_rate": 0,
+        #                 "force_size": "Disabled",
+        #                 "custom_width": 512,
+        #                 "custom_height": 512,
+        #                 "frame_load_cap": 0,
+        #                 "skip_first_frames": 0,
+        #                 "select_every_nth": 1
+        #               },
+        #               "class_type": "VHS_LoadVideoPath",
+        #               "_meta": {
+        #                 "title": "Load Video (Path) 🎥🅥🅗🅢"
+        #               }
+        #             },
+        #             "12": {
+        #               "inputs": {
+        #                 "a": 6.283185307179586,
+        #                 "bg_threshold": 0.1,
+        #                 "resolution": 512,
+        #                 "image": [
+        #                   "10",
+        #                   0
+        #                 ]
+        #               },
+        #               "class_type": "MiDaS-DepthMapPreprocessor",
+        #               "_meta": {
+        #                 "title": "MiDaS Depth Map"
+        #               }
+        #             },
+        #             "14": {
+        #               "inputs": {
+        #                 "filename_prefix": f"{prefix}",
+        #                 "images": [
+        #                   "12",
+        #                   0
+        #                 ]
+        #               },
+        #               "class_type": "SaveImageS3",
+        #               "_meta": {
+        #                 "title": "Save Image to S3"
+        #               }
+        #             }
+        #           }
+        #       }
+        #   }
 
-    #     if file.filename == '':
-    #         flash('No selected title')
-    #         return "Tidak ada namafile"
+        # if file.filename == '':
+        #     flash('No selected title')
+        #     return "Tidak ada namafile"
 
-    #     if file and allowed_file(file.filename):
-    #         output = upload_file_to_s3(file)
+        # if file and allowed_file(file.filename):
+        #     output = upload_file_to_s3(file)
 
-    #         if output:
-    #             flash("Success upload")
-    #             response = requests.post(endpoint_url, headers=headers, data=json.dumps(data))
-    #             if(response.status_code == 200):
-    #                 print("Request Successful: ", response.json())
-    #                 read_file = get_file(); 
-    #                 return render_template('upload_success.html', response_data = response.json(), read_file = read_file)
-    #             else:
-    #                 print(f"Request failed with status code {response.status_code} : {response}")
-    #         else:
-    #             flash("Unable to upload")
-    #             return "Gagal upload"
-    #     else:
-    #         flash("Extension not accepted")
-    #         return "Ekstensi tidak didukung"
+        #     if output:
+        #         flash("Success upload")
+        #         response = requests.post(endpoint_url, headers=headers, data=json.dumps(data))
+        #         if(response.status_code == 200):
+        #             print("Request Successful: ", response.json())
+        #             read_file = get_file(); 
+        #             return render_template('request.html', response_data = response.json(), read_file = read_file)
+        #         else:
+        #             print(f"Request failed with status code {response.status_code} : {response}")
+        #     else:
+        #         flash("Unable to upload")
+        #         return "Gagal upload"
+        # else:
+        #     flash("Extension not accepted")
+        #     return "Ekstensi tidak didukung"
 
     return app
